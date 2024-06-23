d43a8685f46ee615fea6243eb0422e8f6c6040b3
hglee <gjsrl1@gmail.com>
2024-06-23 13:31:27 +0900

    샘플코드 작성

---
+++ javaProgrammingTest/src/com/company/sample/Main.java
@@ -0,0 +1,68 @@
+package com.company.sample;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+
+public class Main {
+    interface MySupplier {
+        Supplier<String> delimiter = () -> ",";
+    }
+    interface MyPredicate extends MySupplier {
+
+    }
+    interface MyFunction extends MyPredicate {
+        void MyFunction();
+
+        Function<String, String> t1 = value -> "return value";
+    }
+    enum MyEnum {
+        _032_INCHEON("032", "Y") {
+            @Override
+            public String getCityName() {
+                return "인천광역시";
+            }
+        };
+
+        /**********************************************************************
+         * 유틸성 메서드
+         **********************************************************************/
+        public static MyEnum getCityByRegionCode(String regionCode) {
+            for (MyEnum e : MyEnum.values()) {
+                if (e.getRegionCode().equals(regionCode)) {
+                    return e;
+                }
+            }
+
+            throw new RuntimeException("존재하지 않는 지역입니다.");
+        }
+        
+        /**********************************************************************
+         * 추상메서드 선언부
+         **********************************************************************/
+        /**
+         * 지역 이름을 반환
+         * @return
+         */
+        public abstract String getCityName();
+
+        /**********************************************************************
+         * getter, constructor
+         **********************************************************************/
+        private String regionCode;
+        private String useYn;
+        public  String getRegionCode() { return regionCode; }
+        public  String getUseYn() { return useYn; }
+        MyEnum(String regionCode, String useYn) {
+            this.regionCode = regionCode;
+            this.useYn      = useYn;
+        }
+    }
+
+    public static void main(String[] args) {
+        String incheon = "032";
+        String cityName = MyEnum.getCityByRegionCode(incheon).getCityName();
+
+        System.out.println("cityName = " + cityName);
+    }
+}
