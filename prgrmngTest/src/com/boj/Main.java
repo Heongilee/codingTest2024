6737396a606e7a3b3234c1b8c6ff919c0da1247f
hglee <gjsrl1@gmail.com>
2024-06-29 14:03:17 +0900

    샘플 자바 파일 업로드

---
+++ prgrmngTest/src/com/boj/Main.java
@@ -0,0 +1,23 @@
+package com.boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    /****************************************************************************************
+     * 백준 ?번 문제
+     *  url         : https://www.acmicpc.net/problem/?
+     ****************************************************************************************/
+    public void solution() throws Exception {
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
